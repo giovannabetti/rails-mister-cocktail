79b4a891f872a982d52ebd6900d0f9df099a826b
Giovanna Betti <giovannabetti@gmail.com>
2020-08-23 20:44:10 -0300

    Cocktails index flow

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read # abre a url e pega o conteudo cru
+ingredients = JSON.parse(ingredients_serialized) # transforma o conteudo cru em algo para trabalharmos
+
+ingredients_array = ingredients['drinks']
+
+ingredients_array.each do |ingredient|
+  ingredient_name = ingredient['strIngredient1']
+  Ingredient.create!(name: ingredient_name)
+end
+
+Cocktail.create!(name: 'Caipirinha')
